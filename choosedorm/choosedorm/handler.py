eb967836aca07d0553aad1212fd74561be76012d
Samuel Deng <mr.dengshuoling@gmail.com>
2016-05-29 22:32:54 +0800

    add logic to protect multi-thread

--- choosedorm/choosedorm/handler.py
+++ choosedorm/choosedorm/handler.py
@@ -37,65 +37,76 @@ def choosedorm(request):
 		return GET()
 
 def POST(chooseInfo):
+        try:
+	        #jugde logical
+	        groupitem = cur.execute("select * from GroupTable where captainID="+chooseInfo['id']+"")
+	        if groupitem==0:
+	        	commitandclose()
+                        # Error Code: 401: 无该组信息
+	        	return HttpResponse("401")
 
-	#jugde logical
-	groupitem = cur.execute("select * from GroupTable where captainID="+chooseInfo['id']+"")
-	if groupitem==0:
-		commitandclose()
-		return HttpResponse("无该组信息".decode('utf8').encode("utf8"))
+	        groupInfos = cur.fetchmany(groupitem)
 
-	groupInfos = cur.fetchmany(groupitem)
-        print groupInfos
-	groupInfo = groupInfos[0]
+                print groupInfos
+                # Error Code: 501: 并发错误
+                if len(groupInfos) != 1:
+                        return HttpResponse("501")
+	        groupInfo = groupInfos[0]
 
-	#if chooseInfo['captain'] != groupInfo[1].decode('utf8'):
-	if chooseInfo['captain'] != groupInfo[1]:
-		commitandclose()
-		return HttpResponse("队长信息有误".decode('utf8').encode("utf8"))
-	if groupInfo[6]!=-1:
-		commitandclose()
-		return HttpResponse("此队已选过，不能再更改".decode('utf8').encode("utf8"))
+	        #if chooseInfo['captain'] != groupInfo[1].decode('utf8'):
+	        if chooseInfo['captain'] != groupInfo[1]:
+	        	commitandclose()
+                        # Error Code 402: 队长信息有误
+	        	return HttpResponse("402")
+	        if groupInfo[6]!=-1:
+	        	commitandclose()
+                        # Error Code: 403:此队已经选过 
+	        	return HttpResponse("403")
 
-	#choose logical
-	dormitem = cur.execute("select * from DormTable where dormID="+chooseInfo['dorm']+"")
-	if dormitem==0:
-		commitandclose()
-		return HttpResponse("无此宿舍".decode('utf8').encode("utf8"))
+	        #choose logical
+	        dormitem = cur.execute("select * from DormTable where dormID="+chooseInfo['dorm']+"")
+	        if dormitem==0:
+	        	commitandclose()
+                        # Error Code: 404:无此宿舍
+	        	return HttpResponse("404")
 
-	dormInfo = cur.fetchmany(dormitem)[0]
+	        dormInfo = cur.fetchmany(dormitem)[0]
 
-	if groupInfo[2] > dormInfo[1]:
-		commitandclose()
-		return HttpResponse("此宿舍床位不够".decode('utf8').encode("utf8"))
+	        if groupInfo[2] > dormInfo[1]:
+	        	commitandclose()
+                        # Error Code: 405:此宿舍床位不够
+	        	return HttpResponse("405")
 
-	#write dorm info
-	#4 member, write directly
-	if groupInfo[2]==4:
-		s = "update DormTable SET remainBed=%s,bed1=%s,bed2=%s,bed3=%s,bed4=%s where dormID="+chooseInfo['dorm']
-		cur.execute(s,(str(dormInfo[1]-groupInfo[2]),groupInfo[1],groupInfo[3],groupInfo[4],groupInfo[5]))
-	#less than 4 member, combine and write
-	else:
-		newMemList = []
-		newMemSum = 4-dormInfo[1]+groupInfo[2]
-		for i in range(0,4-dormInfo[1]):
-			newMemList.append(dormInfo[2+i])
-		newMemList.append(groupInfo[1])
-		for j in range(0,groupInfo[2]-1):
-			newMemList.append(groupInfo[3+j])
-		for k in range(0,4-newMemSum):
-			newMemList.append("")
-		s = "update DormTable SET remainBed=%s,bed1=%s,bed2=%s,bed3=%s,bed4=%s where dormID="+chooseInfo['dorm']
-		#cur.execute("SET NAMES utf8;")
-		cur.execute(s,(str(dormInfo[1]-groupInfo[2]),newMemList[0],newMemList[1],newMemList[2],newMemList[3]))
+	        #write dorm info
+	        #4 member, write directly
+	        if groupInfo[2]==4:
+	        	s = "update DormTable SET remainBed=%s,bed1=%s,bed2=%s,bed3=%s,bed4=%s where dormID="+chooseInfo['dorm']
+	        	cur.execute(s,(str(dormInfo[1]-groupInfo[2]),groupInfo[1],groupInfo[3],groupInfo[4],groupInfo[5]))
+	        #less than 4 member, combine and write
+	        else:
+	        	newMemList = []
+	        	newMemSum = 4-dormInfo[1]+groupInfo[2]
+	        	for i in range(0,4-dormInfo[1]):
+	        		newMemList.append(dormInfo[2+i])
+	        	newMemList.append(groupInfo[1])
+	        	for j in range(0,groupInfo[2]-1):
+	        		newMemList.append(groupInfo[3+j])
+	        	for k in range(0,4-newMemSum):
+	        		newMemList.append("")
+	        	s = "update DormTable SET remainBed=%s,bed1=%s,bed2=%s,bed3=%s,bed4=%s where dormID="+chooseInfo['dorm']
+	        	#cur.execute("SET NAMES utf8;")
+	        	cur.execute(s,(str(dormInfo[1]-groupInfo[2]),newMemList[0],newMemList[1],newMemList[2],newMemList[3]))
 
-	cur.execute("update GroupTable SET dormID="+str(dormInfo[0])+" where captainID="+chooseInfo['id'])
+	        cur.execute("update GroupTable SET dormID="+str(dormInfo[0])+" where captainID="+chooseInfo['id'])
 
-	#search dorm info
-	dormitem = cur.execute("select * from DormTable where dormID="+chooseInfo['dorm'])
-	dormInfo = cur.fetchmany(dormitem)[0]
+	        #search dorm info
+	        dormitem = cur.execute("select * from DormTable where dormID="+chooseInfo['dorm'])
+	        dormInfo = cur.fetchmany(dormitem)[0]
 
-	commitandclose()
-	return HttpResponse("OK")
+	        commitandclose()
+	        return HttpResponse("200")
+        except Exception,e:
+                return HttpResponse("501")
 
 def GET():
 
